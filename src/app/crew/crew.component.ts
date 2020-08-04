7cac6c1423641dd75b1a5cd1a72bcfb9714a5314
Sowmya Bhadraradhya <sowmyab2010@gmail.com>
2020-08-04 00:15:45 -0500

    Studio Part 2 completed.

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -24,6 +24,13 @@ export class CrewComponent implements OnInit {
 
   ngOnInit() { }
 
-  // Code the 'addCrewMember' function here:
+  addCrewMember(candidate) {
+    if (!this.crew.includes(candidate) && this.crew.length < 3) {
+      this.crew.push(candidate);
+    } else if (this.crew.includes(candidate)) {
+      let index = this.crew.indexOf(candidate);
+      this.crew.splice(index, 1);
+    }
+  }
 
 }
