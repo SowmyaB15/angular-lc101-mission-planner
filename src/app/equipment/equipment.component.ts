cb40383f832c8b07741b4f96fbf53f102850e4a5
Sowmya Bhadraradhya <sowmyab2010@gmail.com>
2020-08-05 20:08:19 -0500

    Updated for bonus missions - 2, 3 and 5.

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -27,18 +27,46 @@ export class EquipmentComponent implements OnInit {
 
    ngOnInit() { }
 
+   allowOnlyTwoSameItems(item) {
+     let indices = [];
+     for (let i = 0; i < this.cargoHold.length; i++) {
+       if (this.cargoHold.includes(item)){
+        indices[i] = this.cargoHold.indexOf(item);
+       }
+     }
+     if (indices.length < 2) {
+      this.cargoHold.push(item);
+     }
+   }
    addItem(item): boolean{
-     if (this.cargoMass < this.maximumAllowedMass && this.cargoHold.length < this.maxItems) {
+     if (this.cargoMass < this.maximumAllowedMass && this.cargoHold.length < this.maxItems && this.count(item) < 2) {
       this.cargoHold.push(item);
       this.cargoMass += item.mass;
      } 
      return this.maximumAllowedMass - 200  < this.cargoMass;
    }
    canAddCargo(item) {
-     return this.cargoHold.length !== this.maxItems && this.cargoMass + item.mass <= this.maximumAllowedMass;
+     return this.cargoHold.length !== this.maxItems && this.cargoMass + item.mass <= this.maximumAllowedMass && this.count(item) < 2;
    }
    emptyHold() {
     this.cargoHold = [];
     this.cargoMass = 0;
+   }
+   removeItem(item) {
+     let index = this.cargoHold.indexOf(item);
+     this.cargoHold.splice(index, 1);
+     this.cargoMass -= item.mass; 
+     if(this.cargoHold.length === 0) {
+      this.emptyHold();
+     }
+   }
+   count(item) {
+    let sum = 0;
+    for(let i = 0; i < this.cargoHold.length; i++){
+      if(this.cargoHold[i] === item) {
+        sum++;
+      }
+    }
+    return sum;
    }
 }
